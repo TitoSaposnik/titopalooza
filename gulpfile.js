24fde9213f19de8e380ab56512fb4607b33db479
TitoSaposnik <tito.saposnik@gmail.com>
2022-03-31 00:08:41 -0300

    new commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require('gulp')
+const { src, dest, watch, parallel } = require('gulp')
+
+//CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Img
+const cache = require ('gulp-cache')
+const imagemin = require ('gulp-imagemin')
+const webp = require('gulp-webp')
+const avif = require('gulp-avif')
 
 function css(done){
     src('src/scss/**/*.scss')//Identificqar el archivo .scss a compilar
@@ -9,10 +17,44 @@ function css(done){
         .pipe( dest('build/css') )//Almacenar en el disco duro
     done();
 }
+
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+    .pipe( cache(imagemin(opciones)) )
+    .pipe( dest('build/img'))
+    done();
+}
+
+function versionWebp(done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}') //Busca las imagenes en esos formatos
+    .pipe (webp(opciones))
+    .pipe (dest ('build/img'))
+    done();
+}
+
+function versionAvif(done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}') //Busca las imagenes en esos formatos
+    .pipe (avif(opciones))
+    .pipe (dest ('build/img'))
+    done();
+}
+
 function dev(done){
     watch('src/scss/**/*.scss', css);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
